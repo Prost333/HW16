61e84efbdacdb9cfaf1e4ea12b456a945c57a8de
prost333 <dimabolshoy333@yandex.by>
2023-05-02 10:28:31 +0300

    ty

--- src/Car/Car.java
+++ src/Car/Car.java
@@ -1,28 +1,7 @@
 package Car;
 
 public class Car extends  Thread {
-    private String name;
-    private  int sleep;
-    public Car(String name,int sleep){
-        this.name=name;
-        this.sleep=sleep;
-    }
 
-
-
-    @Override
-    public void run() {
-        Thread thread=new Thread();
-        try {
-            Thread.sleep(sleep);
-            for (int i=0;i<10;i++){
-                System.out.println("машина "+name+" "+i);
-            }
-        } catch (Exception e) {
-           e.printStackTrace();
-        }
-
-    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,4 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
 
-        for (int i=1;i<11;i++){
-            System.out.println("potok"+i+" ");
-            Thread thread=new Thread(new Steam2());
-            thread.start();
-            thread.join();
-        }
-
-    }
-}
+}}
